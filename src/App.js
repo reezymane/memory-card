47f5c76685870877a7f659ea55c776fe49d86a5f
Deonte McCoy <deontemccoy19@gmail.com>
2023-03-31 08:19:41 -0500

    import GameCards

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import React from "react";
+import GameCards from "./components/GameCards";
 
 const App = () => {
   return (
     <div className="App">
       <header>Naruto Memory Cards!</header>
 
-      <div className="GameCards">
-        <div className="Card1"></div>
-
-        <div className="Card2"></div>
-
-        <div className="Card3"></div>
-
-        <div className="Card4"></div>
-
-        <div className="Card5"></div>
-
-        <div className="Card6"></div>
-
-        <div className="Card7"></div>
-      
-        <div className="Card8"></div>
-      
-        <div className="Card9"></div>
-      
-        <div className="Card10"></div>
-      
-        <div className="Card11"></div>
-      
-        <div className="Card12"></div>
-      
-        <div className="Card13"></div>
-      
-        <div className="Card14"></div>
-      
-        <div className="Card15"></div>
-      
-        <div className="Card16"></div>
-      </div>
-
-      
+      <GameCards />
 
       <footer>Copyright <span>&copy;</span> ReezyTech</footer>
     </div>
