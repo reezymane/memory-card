7143f07574d81b09f70a36fb67c28ba3d3f595e8
Deonte McCoy <deontemccoy19@gmail.com>
2023-03-31 08:19:19 -0500

    create GameCards component

---
+++ src/components/GameCards.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export const GameCards = () => {
+    return (
+        <div className="GameCards">
+            <div className="Card1"></div>
+
+            <div className="Card2"></div>
+
+            <div className="Card3"></div>
+
+            <div className="Card4"></div>
+
+            <div className="Card5"></div>
+
+            <div className="Card6"></div>
+
+            <div className="Card7"></div>
+      
+            <div className="Card8"></div>
+      
+            <div className="Card9"></div>
+      
+            <div className="Card10"></div>
+      
+            <div className="Card11"></div>
+      
+            <div className="Card12"></div>
+      
+            <div className="Card13"></div>
+      
+            <div className="Card14"></div>
+      
+            <div className="Card15"></div>
+      
+            <div className="Card16"></div>
+      </div>
+    );
+}
