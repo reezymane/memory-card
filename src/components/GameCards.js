6ab9fd32ab64a65f2d5e53761272c2d761ebdd21
Deonte McCoy <deontemccoy19@gmail.com>
2023-04-03 08:46:01 -0500

    import characters

--- src/components/GameCards.js
+++ src/components/GameCards.js
@@ -1,40 +1,72 @@
 import React from "react";
+import Characters from "./Characters";
 
-const GameCards = () => {
-    
+const GameCards = () => { 
     return (
         <div className="GameCards">
-            <div className="Card1"></div>
+            <div className="Card1">
+                <Characters />
+            </div>
 
-            <div className="Card2"></div>
+            <div className="Card2">
+                <Characters />
+            </div>
 
-            <div className="Card3"></div>
+            <div className="Card3">
+                <Characters />
+            </div>
 
-            <div className="Card4"></div>
+            <div className="Card4">
+                <Characters />
+            </div>
 
-            <div className="Card5"></div>
+            <div className="Card5">
+                <Characters />
+            </div>
 
-            <div className="Card6"></div>
+            <div className="Card6">
+                <Characters />
+            </div>
 
-            <div className="Card7"></div>
+            <div className="Card7">
+                <Characters />
+            </div>
       
-            <div className="Card8"></div>
+            <div className="Card8">
+                <Characters />
+            </div>
       
-            <div className="Card9"></div>
+            <div className="Card9">
+                <Characters />
+            </div>
       
-            <div className="Card10"></div>
+            <div className="Card10">
+                <Characters />
+            </div>
       
-            <div className="Card11"></div>
+            <div className="Card11">
+                <Characters />
+            </div>
       
-            <div className="Card12"></div>
+            <div className="Card12">
+                <Characters />
+            </div>
       
-            <div className="Card13"></div>
+            <div className="Card13">
+                <Characters />
+            </div>
       
-            <div className="Card14"></div>
+            <div className="Card14">
+                <Characters />
+            </div>
       
-            <div className="Card15"></div>
+            <div className="Card15">
+                <Characters />
+            </div>
       
-            <div className="Card16"></div>
+            <div className="Card16">
+                <Characters />
+            </div>
       </div>
     );
 }
