6f2ee8b4e5f3b0d8eb0bf0c2a798a02a9c9f70d1
Deonte McCoy <deontemccoy19@gmail.com>
2023-04-12 07:52:17 -0500

    remove characters state

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,75 +1,10 @@
 import React, { useEffect, useState } from "react";
 
 const Characters = (props) => {
-    const [characters] = useState([{
-        name: "Naruto Uzumaki",
-        img: "naruto.jpg"
-    },
-    {
-        name: "Sasuke Uchiha",
-        img: "sasuke.jpg"
-    },
-    {
-        name: "Sakura Haruno",
-        img: "sakura.jpg"
-    },
-    {
-        name: "Kakashi Hatake",
-        img: "kakashi.jpg"
-    },
-    {
-        name: "Shikamaru Nara",
-        img: "shikamaru.jpg"
-    },
-    {
-        name: "Might Guy",
-        img: "might.jpg"
-    },
-    {
-        name: "Rock Lee",
-        img: "rock.jpg"
-    },
-    {
-        name: "Neji Hyuga",
-        img: "neji.jpg"
-    },
-    {
-        name: "Hinata Hyuga",
-        img: "hinata.jpg"
-    },
-    {
-        name: "Gaara",
-        img: "gaara.jpg"
-    },
-    {
-        name: "Jiraiya",
-        img: "jiraiya.jpg"
-    },
-    {
-        name: "Kisame Hoshigaki",
-        img: "kisame.jpg"
-    },
-    {
-        name: "Itachi Uchiha",
-        img: "itachi.jpg"
-    },
-    {
-        name: "Minato Namikaze",
-        img: "minato.jpg"
-    },
-    {
-        name: "Madara Uchiha",
-        img: "madara.jpg"
-    },
-    {
-        name: "Orochimaru",
-        img: "orochimaru.jpg"
-    }]);
-
     return (
         <div className="CharacterInfo">
-            <p>{characters[props.displayed].name}</p>
-            <p>{characters[props.displayed].img}</p>
+            <p>{props.character.name}</p>
+            <p>{props.character.img}</p>
         </div>
     );
 }
