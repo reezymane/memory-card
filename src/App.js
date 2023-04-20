fa9461c831c7bf6c24425527cbef52e8c3a0844d
Deonte McCoy <deontemccoy19@gmail.com>
2023-04-20 08:29:13 -0500

    create scores state and pass as prop to Scoreboard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import GameCards from "./components/GameCards";
 import Scoreboard from "./components/Scoreboard";
 
 const App = () => {
+  const [scores, setScores] = useState([
+    {
+      currentScore: 0,
+      bestScore: 0
+    }
+  ]);
+  
   return (
     <div className="App">
       <header>
         <div className="title">Naruto Memory Cards!</div>
 
-        <Scoreboard />
+        <Scoreboard
+         scores={scores}
+         setScores={setScores}
+        />
       </header>
 
       <GameCards />
