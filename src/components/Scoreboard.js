abe3306de431859012f815fd17170d64c289f6c5
Deonte McCoy <deontemccoy19@gmail.com>
2023-04-20 08:24:10 -0500

    create and export Scoreboard component

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Scoreboard = (props) => {
+    return (
+        <div className="Scoreboard">
+            <p>Score: {}</p>
+            <p>Best Score: {}</p>
+        </div>
+    );
+};
+
+export default Scoreboard;
