7b6746085f588cbee3987c702ee4e2fe30459a2d
Deonte McCoy <deontemccoy19@gmail.com>
2023-04-20 08:40:58 -0500

    remove array from useState and pass setScores to GameCards

--- src/App.js
+++ src/App.js
@@ -3,25 +3,20 @@ import GameCards from "./components/GameCards";
 import Scoreboard from "./components/Scoreboard";
 
 const App = () => {
-  const [scores, setScores] = useState([
-    {
+  const [scores, setScores] = useState({
       currentScore: 0,
       bestScore: 0
-    }
-  ]);
+    });
   
   return (
     <div className="App">
       <header>
         <div className="title">Naruto Memory Cards!</div>
 
-        <Scoreboard
-         scores={scores}
-         setScores={setScores}
-        />
+        <Scoreboard scores={scores}/>
       </header>
 
-      <GameCards />
+      <GameCards setScores={setScores}/>
 
       <footer>Copyright <span>&copy;</span> ReezyTech</footer>
     </div>
