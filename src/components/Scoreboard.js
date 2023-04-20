05bc71303dfdd73d5c259f8781f64f2b9cc4d71a
Deonte McCoy <deontemccoy19@gmail.com>
2023-04-20 08:39:37 -0500

    add props to p elements

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -3,8 +3,8 @@ import React from "react";
 const Scoreboard = (props) => {
     return (
         <div className="Scoreboard">
-            <p>Score: {}</p>
-            <p>Best Score: {}</p>
+            <p>Score: {props.scores.currentScore}</p>
+            <p>Best Score: {props.scores.bestScore}</p>
         </div>
     );
 };
